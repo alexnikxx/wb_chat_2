2d8d8753c9a0fd1973056531092932229c1b5f03
alexnikxx <alexnikxx@gmail.com>
2024-08-10 10:27:11 +0300

    Initial Commit

---
+++ wb_chat_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wb_chat_2
+//
+//  Created by Aleksandra Nikiforova on 10/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wb_chat_2/wb_chat_2App.swift
@@ -0,0 +1,17 @@
+//
+//  wb_chat_2App.swift
+//  wb_chat_2
+//
+//  Created by Aleksandra Nikiforova on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct wb_chat_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
