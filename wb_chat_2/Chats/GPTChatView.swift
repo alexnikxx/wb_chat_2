201ec49249ada089d6b3fdce9aee10daa7ad31ae
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-08-20 13:39:21 +0300

    set ui for chat GPT

--- wb_chat_2/Chats/GPTChatView.swift
+++ wb_chat_2/Chats/GPTChatView.swift
@@ -11,11 +11,12 @@ import UISystem
 import ExyteChat
 
 struct GPTChatView: View {
+    @Environment(\.colorScheme) var colorScheme
     @EnvironmentObject var router: Router
     @EnvironmentObject private var viewModelGPT: GPTViewModel
     
     var chat: Chat
-
+    
     var body: some View {
         VStack {
             WBNavigationBar(title: chat.title, isBackButton: true, rightButtonIcon: "reload", rightButtonAction: {
@@ -24,9 +25,45 @@ struct GPTChatView: View {
             
             ChatView(messages: viewModelGPT.messages, chatType: .conversation) { draft in
                 viewModelGPT.sendMessage(draftMessage: draft)
+            } inputViewBuilder: { textBinding, attachments, inputViewState, inputViewStyle, inputViewActionClosure, dismissKeyboardClosure in
+                Group {
+                    ZStack {
+                        Rectangle()
+                            .foregroundStyle(Color("background"))
+                            .frame(height: 60)
+                            .shadow(
+                                color: Color("heading2").opacity(colorScheme == .light ? 0.04 : 0),
+                                radius: 12,
+                                x: 0,
+                                y: -1
+                            )
+                        
+                        Rectangle()
+                            .foregroundStyle(Color("background"))
+                            .frame(height: 60)
+                            .offset(x: 0, y: 40)
+                        
+                        HStack(spacing: 12) {
+                            WBTextField(placeholder: "Аа", text: textBinding)
+                            
+                            Button {
+                                inputViewActionClosure(.send)
+                            } label: {
+                                Image("send-alt-filled")
+                            
+                                
+                            }
+                            .disabled(textBinding.wrappedValue.isEmpty)
+                        }
+                        .padding(.horizontal)
+                    }
+                }
             }
-            .messageUseMarkdown(messageUseMarkdown: true)
-            .padding(.vertical, 20)
+            .chatTheme(colors: ChatTheme.Colors(
+                mainBackground: .textfield,
+                myMessage: .accent,
+                friendMessage: .background
+            ))
         }
         .onAppear {
             viewModelGPT.switchToChat(chat)
